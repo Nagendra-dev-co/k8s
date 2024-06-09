1a8697987a451531e061986f4a1a2becc62acdc8
Nagendra-dev-co <111262473+Nagendra-dev-co@users.noreply.github.com>
2024-06-09 11:23:48 +0530

    Update edit-employee.php

--- edit-employee.php
+++ edit-employee.php
@@ -1,10 +1,10 @@
 <?php 
 
      //connect to database
-     $servername = "database-1.cdaqmc6max1u.us-east-1.rds.amazonaws.com";
-     $username = "admin";
-     $password = "root12345";
-     $database = "php";
+     $servername = "localhost";
+     $username = "root";
+     $password = "";
+     $database = "php_employee_management";
  
      //Create Connection
      $connection = new mysqli($servername, $username, $password, $database);
@@ -122,25 +122,25 @@
 
 /* for showing validation msg red */
 *{
-        padding: 0px;
-        margin:0px;
+	padding: 0px;
+	margin:0px;
 }
 .navbar{
-        margin: 0px;
+	margin: 0px;
 }
 .error {
-        color: red;
-        font-style: italic;
+	color: red;
+	font-style: italic;
 }
 
 .mycolor{
-        background: #748EC6;
+	background: #748EC6;
 }
 .text-w{
-        color:#748EC6;
+	color:#748EC6;
 }
 .background-color-w{
-        color:#F2F5FA;
+	color:#F2F5FA;
 }
 
 .section-bg {
@@ -168,37 +168,37 @@
 
 <!-- navbar start -->
 <nav class="navbar navbar-expand-lg navbar-dark mycolor">
-                <a class="navbar-brand" href="index.php">Employee Management</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                </button>
+		<a class="navbar-brand" href="index.php">Employee Management</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent" aria-expanded="false"
+			aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                                <li class="nav-item active"><a class="nav-link" href="index.php">Home
-
-                                </a></li>
-                                <li class="nav-item active"><a class="nav-link" href="./create-new-employee.php">Add
-                                                Employee</a></li>
-                                <li class="nav-item dropdown"><a
-                                        class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false"> Dropdown </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item" href="#">Action</a> <a
-                                                        class="dropdown-item" href="#">Another action</a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                        </div></li>
-                                        <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a>
-                                </li>
-                        </ul>
-
-                </div>
-        </nav>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+				<li class="nav-item active"><a class="nav-link" href="index.php">Home
+						
+				</a></li>
+				<li class="nav-item active"><a class="nav-link" href="./create-new-employee.php">Add
+						Employee</a></li>
+				<li class="nav-item dropdown"><a
+					class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+					role="button" data-toggle="dropdown" aria-haspopup="true"
+					aria-expanded="false"> Dropdown </a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Action</a> <a
+							class="dropdown-item" href="#">Another action</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div></li>
+					<li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+			</ul>
+			
+		</div>
+	</nav>
 
 <!-- navbar End -->
 
